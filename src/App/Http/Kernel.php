1708783d1404c9d9a1e27f2d317a6743fb4143e1
andikaryanto <41881539+andikaryanto@users.noreply.github.com>
2022-09-02 10:26:20 +0700

    hanlde custom request

--- src/App/Http/Kernel.php
+++ src/App/Http/Kernel.php
@@ -19,4 +19,18 @@ class Kernel extends HttpKernel
             Request::createFrom($request)
         );
     }
+    
+    /**
+     * flexible create request from any custom request class
+     *
+     * @param string $requestClass
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @return void
+     */
+    public function handleRequest(string $requestClass, $request){
+        return parent::handle(
+            $requestClass::createFrom($request)
+        );
+    }
 }
