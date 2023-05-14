3da763a2823499435403612578e8f6857e33c8c3
Andik Aryanto <andik.aryantoo@gmail.com>
2023-05-14 14:33:39 +0700

    new model

---
+++ src/App/Models/TraitAuditableModel.php
@@ -0,0 +1,86 @@
+<?php
+
+use Carbon\Carbon;
+
+trait TraitAuditableModel
+{
+    /**
+     * Get the value of created_by
+     */ 
+    public function getCreatedBy(): ?string
+    {
+        return $this->created_by;
+    }
+
+    /**
+     * Set the value of created_by
+     *
+     * @return  self
+     */ 
+    public function setCreatedBy(?string $createdBy)
+    {
+        $this->created_by = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_by
+     */ 
+    public function getUpdatedBy(): ?string
+    {
+        return $this->updated_by;
+    }
+
+    /**
+     * Set the value of updated_by
+     *
+     * @return  self
+     */ 
+    public function setUpdatedBy(?string $updatedBy)
+    {
+        $this->updated_by = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */ 
+    public function getCreatedAt(): ?Carbon
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @return  self
+     */ 
+    public function setCreatedAt(?Carbon $createdAt)
+    {
+        $this->created_at = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     */ 
+    public function getUpdatedAt(): ?Carbon
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @return  self
+     */ 
+    public function setUpdatedAt(?Carbon $updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+
+        return $this;
+    }
+}
--- src/App/Models/User.php
+++ src/App/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use LaravelCommon\App\Models\User\ScopeMapping;
+use TraitAuditableModel;
 
 // use Laravel\Sanctum\HasApiTokens;
 
@@ -17,6 +18,14 @@ class User extends Authenticatable
     // use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use TraitAuditableModel;
+
+    protected string $username;
+    // protected Groupuser $groupuser;
+    protected string $email;
+    protected string $password;
+    protected ?string $photo = null;
+    protected bool $is_active = true;
 
     /**
      * The attributes that are mass assignable.
@@ -52,7 +61,7 @@ class User extends Authenticatable
      *
      * @return BelongsTo
      */
-    public function groupuser()
+    private function groupuser()
     {
         return $this->belongsTo(Groupuser::class);
     }
@@ -61,8 +70,102 @@ class User extends Authenticatable
      *
      * @return HasMany
      */
-    public function scopeMappings()
+    private function scopeMappings()
     {
         return $this->hasMany(ScopeMapping::class);
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     *
+     * @param string|null $username
+     * @return $this
+     */
+    public function setUsername(?string $username): User
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail(string $email): User
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword(string $password): User
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of is_active
+     */ 
+    public function getIsActive(): bool
+    {
+        return $this->is_active;
+    }
+
+    /**
+     * Set the value of is_active
+     *
+     * @return  self
+     */ 
+    public function setIsActive($isActive): User
+    {
+        $this->is_active = $isActive;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function getGroupuser()
+    {
+        $this->groupuser;
+    }
+
+    public function setGroupuser(Groupuser $groupuser)
+    {
+        $this->groupuser()->associate($groupuser);
     }
 }
