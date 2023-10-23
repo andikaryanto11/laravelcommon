546296531c597c0845fb2cf48b141cca79744bc6
andikaryanto <41881539+andikaryanto@users.noreply.github.com>
2023-10-23 22:00:34 +0700

    - redesign model in model context only

---
+++ src/App/Queries/GroupuserQuery.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace LaravelCommon\App\Queries;
+
+use Carbon\Carbon;
+use DateTime;
+use Illuminate\Database\ConnectionInterface;
+use LaravelCommon\App\Queries\Query;
+use Illuminate\Database\Query\Processors\Processor;
+use Illuminate\Database\Query\Grammars\Grammar;
+use LaravelCommon\App\Models\Groupuser;
+use LaravelCommon\App\Models\Scope;
+use LaravelCommon\App\ViewModels\GroupuserCollection;
+use LaravelCommon\App\ViewModels\UserCollection;
+
+class GroupuserQuery extends Query
+{
+    /**
+     * Create a new query builder instance.
+     *
+     * @param  Groupuser  $groupuser
+     * @param  \Illuminate\Database\ConnectionInterface  $connection
+     * @param  \Illuminate\Database\Query\Grammars\Grammar|null  $grammar
+     * @param  \Illuminate\Database\Query\Processors\Processor|null  $processor
+     * @return void
+     */
+    public function __construct(
+        Groupuser $groupuser,
+        ConnectionInterface $connection,
+        Grammar $grammar = null,
+        Processor $processor = null
+    ) {
+        parent::__construct($groupuser, $connection, $grammar, $processor);
+    }
+
+    public function collectionClass()
+    {
+        return GroupuserCollection::class;
+    }
+
+    public function whereUserCreatedBefore(Carbon $date): GroupuserQuery
+    {
+        $this->joinWith('users', 'groupuser.id', '=', 'users.groupuser_id')
+            ->where('users.created_at', '<', $date->format('Y-m-d H:i:s'));
+        return $this;
+    }
+
+    public function whereUserCreatedAfter(Carbon $date): GroupuserQuery
+    {
+        $this->joinWith('users', 'groupusers.id', '=', 'users.groupuser_id')
+            ->where('users.created_at', '>', $date->format('Y-m-d H:i:s'));
+        return $this;
+    }
+
+    public function whereScope(Scope $scope): GroupuserQuery
+    {
+
+        $this->joinWith('users', 'groupusers.id', '=', 'users.groupuser_id')
+            ->joinWith('user_scopes', 'users.id', '=', 'user_scopes.user_id')
+            ->joinWith('scopes', 'user_scopes.scope_id', '=', 'scopes.id')
+            ->where('scopes.id', '=', $scope->getId());
+
+        return $this;
+    }
+}
---
+++ src/App/Routes/GroupuserRoute.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LaravelCommon\App\Routes;
+
+use Illuminate\Support\Facades\Route;
+use LaravelCommon\App\Http\Controllers\GroupuserController;
+
+class GroupuserRoute extends CommonRoute {
+    public static function register()
+    {
+        Route::prefix('api')->group(function () {
+            Route::get('/groupusers', [GroupuserController::class, 'getAll']);
+        });
+    }
+}
