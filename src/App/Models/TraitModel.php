86b0c4235f254e68bceed3eeadc6ece16560aefb
Andik <andik.aryantoo@gmail.com>
2023-11-18 23:40:58 +0700

    is equal to

--- src/App/Models/TraitModel.php
+++ src/App/Models/TraitModel.php
@@ -109,36 +109,8 @@ trait TraitModel
         return $this;
     }
 
-    // /**
-    //  * Instantiate a new BelongsToMany relationship.
-    //  *
-    //  * @param  \Illuminate\Database\Eloquent\Builder  $query
-    //  * @param  \Illuminate\Database\Eloquent\Model  $parent
-    //  * @param  string  $table
-    //  * @param  string  $foreignPivotKey
-    //  * @param  string  $relatedPivotKey
-    //  * @param  string  $parentKey
-    //  * @param  string  $relatedKey
-    //  * @param  string|null  $relationName
-    //  * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-    //  */
-    // protected function newBelongsToMany(
-    //     Builder $query,
-    //     Model $parent,
-    //     $table,
-    //     $foreignPivotKey,
-    //     $relatedPivotKey,
-    //     $parentKey,
-    //     $relatedKey,
-    //     $relationName = null
-    // ) {
-    //     return new BelongsToMany(
-    //         $query, $parent, $table, $foreignPivotKey, $relatedPivotKey, $parentKey, $relatedKey, $relationName
-    //     );
-    // }
-
-    public function isEqualTo(Model $model)
-    {
-        return $this->getId() == $model->getId();
+    public function isEqualTo(Model $model) {
+        return get_class($this) == get_class($model) &&
+            $this->getId() == $model->getId();
     }
 }
